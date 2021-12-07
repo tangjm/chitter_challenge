89d876104ce752ebe1d33a19d0ad1c75480301f8
tangjm <jaredtang@hotmail.co.uk>
2021-12-07 17:52:44 +0000

    Added allPeeps route for handle GET requests for all peeps

--- backend/routes/allPeeps.js
+++ backend/routes/allPeeps.js
@@ -7,5 +7,8 @@ router.route(`/`)
 		// get request to retrieve all stored peeps in database
 		// Use Peep model to query database's peeps collection for all peeps
 		// Then send back the peeps as a json object
-		Peep.find()
+		Peep.find((err, peeps) => {
+			err ? res.status(400).json({ "message": "no peeps found" }) :
+				res.json(peeps);
+		})
 	})
--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 // routes
 // app.use(`/`, homeRoute);
-// app.use(`/`, allPeepsRoute);
+app.use(`/allPeeps`, allPeepsRoute);
 
 
 const main = async () => {
