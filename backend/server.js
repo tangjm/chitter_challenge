2ea249d3d0538373b8a4064c9ef3e7853f7ed524
tangjm <jaredtang@hotmail.co.uk>
2021-12-07 17:29:47 +0000

    Added cors and express.json() to server.js

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,16 @@
 require('dotenv').config({ path: `.env.${process.env.NODE_ENV}` });
+const cors = require('cors');
 const express = require('express');
 
 const host = process.env.HOST;
 const port = process.env.PORT;
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// routes
+
 
 const server = app.listen(port, host, () => {
 	const serverHOST = server.address().address;
