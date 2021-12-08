4eb94565a0e3a239050840d005d1e87b825b9da7
tangjm <jaredtang@hotmail.co.uk>
2021-12-08 21:22:10 +0000

    Finished static version

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from '../src/Components/Header';
 import AddPeep from './Components/AddPeep';
 import AllPeeps from './Components/AllPeeps';
@@ -13,9 +14,15 @@ function App() {
   return (
     <div className="App-header">
       <Header />
-      <AddPeep />
-      <AllPeeps />
-    </div>
+      &nbsp;
+      <Router>
+        <Routes>
+          <Route path="/" element={<AllPeeps />} />
+          <Route path="/addPeep" element={<AddPeep />} />
+        </Routes>
+      </Router>
+
+    </div >
   );
 }
 
--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -8,9 +8,9 @@ const Header = () => {
 		<header>
 			<Navbar bg="primary" variant="dark">
 				<Container style={{ width: '70rem' }}>
-					<Navbar.Brand href="#home">Chitter</Navbar.Brand>
+					<Navbar.Brand href="/" style={{ "font-size": "100%" }}>Chitter</Navbar.Brand>
 					<Nav className="me-auto">
-						<Nav.Link href="#home">Home</Nav.Link>
+						<Nav.Link href="/addPeep">New Peep</Nav.Link>
 						<Nav.Link href="#features">Login</Nav.Link>
 						<Nav.Link href="#pricing">Register</Nav.Link>
 					</Nav>
