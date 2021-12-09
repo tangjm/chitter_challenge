93e02085a216974568012dce369a33486506f9f9
tangjm <jaredtang@hotmail.co.uk>
2021-12-09 19:31:52 +0000

    Added form manipulation tests for AddPeep component

--- frontend/src/tests/AddPeep.test.js
+++ frontend/src/tests/AddPeep.test.js
@@ -1,32 +1,56 @@
 import AddPeep from '../Components/AddPeep';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-describe(`Tests for AddPeep`, () => {
-	test(`it should render a label`, () => {
+
+describe(`Test suite for AddPeep`, () => {
+	beforeEach(() => {
 		render(<Router>
 			<AddPeep />
 		</Router>);
-		const labelText = /your peep:/i;
-		const actual = screen.getByLabelText(labelText);
-		expect(actual).toBeInTheDocument();
 	})
 
-	test(`it should render textarea`, () => {
-		render(<Router>
-			<AddPeep />
-		</Router>);
-		const placeholderText = /Your message.../i;
-		const actual = screen.getByPlaceholderText(placeholderText);
-		expect(actual).toBeInTheDocument();
+	describe(`Render tests for AddPeep`, () => {
+		test(`it should render a label`, () => {
+			const labelText = /your peep:/i;
+			const actual = screen.getByLabelText(labelText);
+			expect(actual).toBeInTheDocument();
+		})
+
+		test(`it should render textarea`, () => {
+			const placeholderText = /Your message.../i;
+			const actual = screen.getByPlaceholderText(placeholderText);
+			expect(actual).toBeInTheDocument();
+		})
+
+		test(`it should render an input button`, () => {
+			const actual = screen.getByRole("button");
+			expect(actual).toBeInTheDocument();
+		})
 	})
 
-	test(`it should render an input button`, () => {
-		render(<Router>
-			<AddPeep />
-		</Router>);
+	describe(`Form manipulation tests`, () => {
+		test(`it should register changes to the textarea`, () => {
+			const testMessage = "test peep message";
+			const placeholderText = /your message.../i;
+			const textArea = screen.getByPlaceholderText(placeholderText);
+			userEvent.type(textArea, testMessage);
 
-		const actual = screen.getByRole("button");
-		expect(actual).toBeInTheDocument();
+			expect(textArea).toHaveValue(testMessage);
+		})
+	})
+
+	describe(`Peep submission tests for AddPeep`, () => {
+		test(`it should call`, () => {
+			const labelText = /your peep:/i;
+			const actual = screen.getByLabelText(labelText);
+			expect(actual).toBeInTheDocument();
+		})
+
 	})
 })
+
+
+
+
