31105ec6dcacbad3e9418c3f3315149a78c93658
tangjm <jaredtang@hotmail.co.uk>
2021-12-09 16:20:28 +0000

    Added routes and state for user

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import axios from 'axios';
+import { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from '../src/Components/Header';
 import AddPeep from './Components/AddPeep';
 import AllPeeps from './Components/AllPeeps';
 
+import Login from './Components/UserValidation/Login';
+import Register from './Components/UserValidation/Register';
+
 function App() {
+  const [user, setUser] = useState({
+    "name": "anonymous",
+    "username": "anon"
+  });
 
-
+  const port = process.env.REACT_APP_PORT;
+  const host = process.env.REACT_APP_HOST;
+  const baseUrl = `http://${host}:${port}`;
 
 
   return (
@@ -17,8 +27,10 @@ function App() {
       &nbsp;
       <Router>
         <Routes>
-          <Route path="/" element={<AllPeeps />} />
-          <Route path="/addPeep" element={<AddPeep />} />
+          <Route path="/" element={<AllPeeps baseUrl={baseUrl} />} />
+          <Route path="/addPeep" element={<AddPeep baseUrl user={user} />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </Router>
 
