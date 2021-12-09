dd88497dd0a32f48ecc2282814229e1ee1b9f894
tangjm <jaredtang@hotmail.co.uk>
2021-12-09 16:22:02 +0000

    Created component for posting new peeps

--- frontend/src/Components/AddPeep.jsx
+++ frontend/src/Components/AddPeep.jsx
@@ -1,15 +1,58 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import axios from 'axios';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import { useNavigate } from 'react-router-dom';
 
-const AddPeep = () => {
+const AddPeep = ({ baseUrl, user }) => {
+	const [message, setMessage] = useState(``);
+	const [date, setDate] = useState(``);
+
+	let navigate = useNavigate();
+	let path = `${baseUrl}/addPeep`;
+	const placeholderText = "Your message..."
+
+	const postPeep = async () => {
+		try {
+			const res = await axios.post(path, {
+				"message": message,
+				"sender": user,
+				"date": date,
+				"metaData": {
+					isReply: false
+				}
+			})
+			if (res.status === 200) {
+				return navigate("/");
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	const submitHandler = event => {
+		event.preventDefault();
+		// ensure message is not an empty string
+		if (message) {
+			const dateOfPeep = new Date().toISOString();
+			setDate(dateOfPeep);
+			return postPeep();
+		}
+		// render something to tell the user to enter a valid message
+	}
 
 	return (
 		<div>
-			<Form style={{ width: '30rem' }} >
+			<Form className="singlePeep" onSubmit={submitHandler}>
 				<Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
 					<Form.Label>Your peep:</Form.Label>
-					<Form.Control as="textarea" rows={3} />
+					<Form.Control
+						as="textarea"
+						placeholder={placeholderText} rows={3}
+						onChange={event => setMessage(event.target.value)}
+					// isInvalid={}
+					/>
 				</Form.Group>
 				<Button as="input" type="submit" value="Post" />{' '}
 			</Form>
@@ -17,4 +60,12 @@ const AddPeep = () => {
 	)
 }
 
+
+AddPeep.propTypes = {
+	baseUrl: PropTypes.string,
+	user: PropTypes.exact({
+		"name": PropTypes.string,
+		"username": PropTypes.string
+	})
+}
 export default AddPeep;
