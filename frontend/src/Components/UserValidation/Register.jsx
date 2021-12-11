b10a28d420fdfb2d3b3458af00f114524b7755c3
tangjm <jaredtang@hotmail.co.uk>
2021-12-11 11:58:54 +0000

    Added form validation; Successfully sends post requests to server

--- frontend/src/Components/UserValidation/Register.jsx
+++ frontend/src/Components/UserValidation/Register.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
@@ -7,31 +9,66 @@ import Row from 'react-bootstrap/Row';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import InputGroup from 'react-bootstrap/InputGroup';
 
-const Register = () => {
+const Register = ({ baseUrl }) => {
+	// const [validated, setValidated] = useState(false);
 	const [name, setName] = useState(``);
 	const [username, setUsername] = useState(``);
 	const [email, setEmail] = useState(``);
 	const [password, setPassword] = useState(``);
+	const navigate = useNavigate();
+
+	const registerUser = async () => {
+		try {
+			const res = await axios.post(`${baseUrl}/register`, {
+				name: name,
+				username: username,
+				email: email,
+				password: password
+			})
+			if (res.status === 200) {
+				return navigate(`/login`);
+			}
+			if (res.status === 400) {
+
+			}
+		} catch (err) {
+			console.log(err);
+		}
+	}
+
+	const handleSubmit = event => {
+		event.preventDefault();
+		registerUser();
+	}
+
 
 	return (
 		<>
 			<h2 className="mb-4">Sign Up!</h2>
-			<Form>
+			<Form onSubmit={handleSubmit}>
 				<Row className="align-items-center">
 					<Col xs="auto">
 						<InputGroup className="mb-4" hasValidation>
 							<FloatingLabel className="floatingLabel" controlId="floatingInputGrid" label="Name">
-								<Form.Control type="text" placeholder="Name" onChange={event => setName(event.target.value)} />
+								<Form.Control type="text" placeholder="Name" onChange={event => setName(event.target.value)}
+									required
+									isInvalid={false} />
+								{/* Work in progess: custom validation error messages and css */}
+								<Form.Control.Feedback type="invalid" style={{ color: "red" }}>
+									Please choose a name.
+								</Form.Control.Feedback>
 							</FloatingLabel>
+
 						</InputGroup>
 					</Col>
 
 					<Col xs="auto">
-						<InputGroup className="mb-4" hasValidation>
+						<InputGroup className="mb-4">
 							<InputGroup.Text>@</InputGroup.Text>
 							<FloatingLabel className="floatingLabel" controlId="floatingSelectGrid" label="Username">
 								<Form.Control type="text" placeholder="Username"
 									onChange={event => setUsername(event.target.value)}
+									required
 								/>
 							</FloatingLabel>
 						</InputGroup>
@@ -43,7 +80,8 @@ const Register = () => {
 						<InputGroup className="mb-4">
 							<FloatingLabel className="floatingLabel" controlId="floatingEmailLabel" label="Email">
 								<Form.Control type="email" placeholder="Email"
-									onChange={event => setEmail(event.target.value)} />
+									onChange={event => setEmail(event.target.value)}
+									required />
 							</FloatingLabel>
 						</InputGroup>
 					</Col>
@@ -51,7 +89,8 @@ const Register = () => {
 						<InputGroup className="mb-4">
 							<FloatingLabel className="floatingLabel" controlId="floatingInputGrid" label="Password">
 								<Form.Control type="password" placeholder="Password"
-									onChange={event => setPassword(event.target.value)} />
+									onChange={event => setPassword(event.target.value)}
+									required />
 							</FloatingLabel>
 						</InputGroup>
 					</Col>
@@ -66,4 +105,7 @@ const Register = () => {
 	)
 }
 
+Register.propTypes = {
+	baseUrl: PropTypes.string,
+}
 export default Register;
