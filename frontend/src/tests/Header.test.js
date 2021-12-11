d4be3dbc714ee6499251b86fee7462478a4244a2
tangjm <jaredtang@hotmail.co.uk>
2021-12-11 12:34:08 +0000

    removed header snapshot test as header is no longer static

--- frontend/src/tests/Header.test.js
+++ frontend/src/tests/Header.test.js
@@ -1,7 +1,3 @@
 import Header from '../Components/Header';
 import { render } from '@testing-library/react';
 
-test(`Header matches snapshot`, () => {
-	const HeaderComponent = render(<Header />);
-	expect(HeaderComponent).toMatchSnapshot();
-})
