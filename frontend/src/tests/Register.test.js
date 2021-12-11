717e614907966ef6bdb8c1ad5eba622f02084c2b
tangjm <jaredtang@hotmail.co.uk>
2021-12-11 15:38:22 +0000

    Updated tests for Register component

--- frontend/src/tests/Register.test.js
+++ frontend/src/tests/Register.test.js
@@ -2,6 +2,7 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Register from '../Components/UserValidation/Register';
 import { BrowserRouter as Router } from 'react-router-dom';
+
 describe(`Test suite for Register component`, () => {
 	beforeEach(() => {
 		render(
